67192e3b37d8223bca2cae9ac03c3bba56930f21
LiYang <1499271883@qq.com>
2021-12-10 09:24:12 +0800

    定义路由

--- Mapbox/mapbox_with_vue/src/main.js
+++ Mapbox/mapbox_with_vue/src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from "vue-router";
+import routers from "@/router";
 
-Vue.config.productionTip = false
+Vue.use(VueRouter)
 
+const router = new VueRouter({
+  mode:'history',
+  routes:routers
+})
 new Vue({
+  el:'#app',
+  router,
   render: h => h(App),
-}).$mount('#app')
+})
---
+++ Mapbox/mapbox_with_vue/src/router.js
@@ -0,0 +1,15 @@
+import DisplayAMap from "@/view/displayAMap";
+import HelloWorld from "@/components/HelloWorld";
+
+const routers = [
+    {
+        path:'displayAMap',
+        name:'displayAMap',
+        component:DisplayAMap
+    },
+    {
+        path: '/',
+        component: HelloWorld
+    }
+]
+export default routers
