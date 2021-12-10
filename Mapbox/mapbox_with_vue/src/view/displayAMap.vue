3afcb8ebd1335f3b229bb9abb23562c0a035e625
LiYang <1499271883@qq.com>
2021-12-10 09:56:49 +0800

    添加展示地图的demo

---
+++ Mapbox/mapbox_with_vue/src/view/displayAMap.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div id="map"></div>
+  </div>
+</template>
+
+<script>
+import mapConfig from "../../config/mapConfig";
+import mapboxgl from 'mapbox-gl'
+export default {
+  name: "DisplayAMap",
+  mounted() {
+    this.init()
+  },
+  methods:{
+    init(){
+      mapboxgl.accessToken = mapConfig.access_token
+      new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/streets-v11', // style URL
+        center: [-74.5, 40], // starting position [lng, lat]
+        zoom: 9 // starting zoom
+      });
+    }
+  }
+}
+</script>
+
+<style scoped>
+#map { position: absolute; top: 0; bottom: 0; width: 90%;height: 90%;margin: 2.5% 5% 5% 5%}
+</style>
