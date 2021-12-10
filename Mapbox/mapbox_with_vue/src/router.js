95de058f0f5e640ad84db1a286ed6fbb0339dc48
LiYang <1499271883@qq.com>
2021-12-10 10:45:33 +0800

    添加地形demo

--- Mapbox/mapbox_with_vue/src/router.js
+++ Mapbox/mapbox_with_vue/src/router.js
@@ -1,11 +1,17 @@
 import DisplayAMap from "@/view/displayAMap";
 import HelloWorld from "@/components/HelloWorld";
+import addTerrain from "@/components/addTerrain";
 
 const routers = [
     {
         path:'/displayAMap',
         name:'displayAMap',
         component:DisplayAMap
+    },
+    {
+        path:'/addTerrain',
+        name:'addTerrain',
+        component:addTerrain
     },
     {
         path: '/',
